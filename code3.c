f87670abd8e730a2429fa3069b381bf9518b0647
Chaitanya696969 <161908372+Chaitanya696969@users.noreply.github.com>
2025-07-03 19:22:27 +0530

    code3.c

---
+++ code3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+int main() {  //main function  
+    int numbers[4] = {10, 20, 30, 40};  
+    printf("Accessing array elements:\n");  
+    printf("Element at index 0: %d\n", numbers[0]);  
+    printf("Element at index 1: %d\n", numbers[1]);  
+    printf("Element at index 2: %d\n", numbers[2]);  
+    printf("Element at index 3: %d\n", numbers[3]);  
+    return 0;  
+} 
